f688183a6e871b38f9d7194d404e2b17bcd72dab
Hayden Johnson <hajohn100@gmail.com>
2025-10-22 09:12:35 -0700

    update configure.sh for more commands

--- configure.sh
+++ configure.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Command to run wp-cli in docker
-WP_CLI="docker exec -it aga-congress-2026-wp-cli-1 wp"
+CLI_CONTAINER="aga-congress-2026-wp-cli-1"
+
+BASE_CMD="docker exec -it $CLI_CONTAINER "
 
 # Install core site with default credentials
-$WP_CLI core install --url=http://localhost --title="U.S. Go Congress" --admin_user=admin --admin_password=password --admin_email=admin@admin.example
+$BASE_CMD wp core install --url=http://localhost --title="U.S. Go Congress" --admin_user=admin --admin_password=password --admin_email=admin@admin.example
 
 # Import our pages
-$WP_CLI plugin install wordpress-importer --activate
-$WP_CLI import /import_data/base_pages.xml --authors=create
+$BASE_CMD wp plugin install wordpress-importer --activate
+$BASE_CMD cp /import_data/base_pages.xml /var/www/html/base_bages.xml
+$BASE_CMD wp import /var/www/html/base_pages.xml --authors=create
 
 # Install kadence theme and plugins
-$WP_CLI theme install kadence --activate
-$WP_CLI plugin install stackable-ultimate-gutenberg-blocks --activate
-$WP_CLI plugin install wp-super-cache --activate
+$BASE_CMD wp theme install kadence --activate
+$BASE_CMD wp plugin install stackable-ultimate-gutenberg-blocks --activate
+$BASE_CMD wp plugin install wp-super-cache --activate
 
 # Make front page show a static page instead of blog posts, and set it to our home page
-$WP_CLI option update show_on_front 'page'
-$WP_CLI option update page_on_front 5
+$BASE_CMD wp option update show_on_front 'page'
+$BASE_CMD wp option update page_on_front 5
